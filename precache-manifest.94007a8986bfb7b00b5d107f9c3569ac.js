4282955eea6ca8b308723ad8cea12e2992760d8a
Daria Sukhova <d.es.tiny.1234545@gmail.com>
2021-05-25 02:02:04 +0300

    Updates

---
+++ precache-manifest.94007a8986bfb7b00b5d107f9c3569ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf42b6396d9213ea13502840313f96ee",
+    "url": "/crud-ui/index.html"
+  },
+  {
+    "revision": "46b014f289671b449e95",
+    "url": "/crud-ui/static/css/main.2d719117.chunk.css"
+  },
+  {
+    "revision": "e5e47a4f2057fedc28d3",
+    "url": "/crud-ui/static/js/2.7b60ed6a.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/crud-ui/static/js/2.7b60ed6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46b014f289671b449e95",
+    "url": "/crud-ui/static/js/main.b7298d7e.chunk.js"
+  },
+  {
+    "revision": "58d805d3210bea99ca34",
+    "url": "/crud-ui/static/js/runtime-main.6729c521.js"
+  }
+]);
